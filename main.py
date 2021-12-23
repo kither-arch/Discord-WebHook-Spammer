cbffd8218a111b8631a2445f943afa5959c3ca73
kither-arch <83691457+kither-arch@users.noreply.github.com>
2021-12-23 19:10:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 import pyfiglet
 banner = pyfiglet.figlet_format("KINGMAN")
 print(banner)
-msg = input("Please Insert WebHook Spam Message: ")
-webhook = input("Please Insert WebHook URL: ")
+msg = input(" I LOVE YOU DEV: ")
+webhook = input("https://discord.com/api/webhooks/923568259205320704/r3GOWrh6ims_dYXbp5kYbsJy1roaWFFxOlTN-LczIo19wEA54ZEfoSgiTGK-i7xPG3f0 : ")
 def spam(msg, webhook):
     while True:
         try:
